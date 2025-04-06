12949d0187689bab0cd8dcc54f2e5351259e21a0
NikitaKx9 <nikitakx9@gmail.com>
2025-04-06 15:50:46 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
 import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import useTelergam from './hooks/useTelergam'
 import Header from './components/Header/Header';
+import Form from './components/Form/Form';
+import ProductList from './components/ProductList/ProductList';
+import Button from './components/Button/Button';
 
 
 function App() {
@@ -15,7 +19,13 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Router basename='https://nikitakozhukhar.github.io/tg_web_app_react/'> 
+        <Routes>
+          <Route path='/form' element={<Form/>}/>
+          <Route path='/productlist' element={<ProductList/>}/>
+        </Routes>
+        <Button onClick={onToggleButton}>toggle</Button>
+      </Router>
     </div>
   )
 }
